c2cc1f69dda15c9ad44c86aafa86942249a72d7d
Todd Kennedy <todd@updater.com>
2012-12-02 22:29:18 -0500

    styled, first post

--- plugins/blog.py
+++ plugins/blog.py
@@ -7,54 +7,56 @@ POSTS = []
 
 def preBuild(site):
 
-	global POSTS
+    global POSTS
 
-	# Build all the posts
-	for page in site.pages():
-		if page.path.startswith(POSTS_PATH):
+    # Build all the posts
+    for page in site.pages():
+        if page.path.startswith(POSTS_PATH):
 
-			if not page.path.endswith('.html'):
-				continue
+            if not page.path.endswith('.html'):
+                continue
 
-			def find(name):
-				c = page.context()
-				if not name in c:
-					logging.info("Missing info '%s' for post %s" % (name, page.path))
-					return ''
-				return c.get(name, '')
+            def find(name):
+                c = page.context()
+                if not name in c:
+                    logging.info("Missing info '%s' for post %s" % (name, page.path))
+                    return ''
+                return c.get(name, '')
 
-			postContext = {}
-			postContext['title'] = find('title')
-			postContext['author'] = find('author')
-			postContext['date'] = find('date')
-			postContext['path'] = page.path
 
-			# Parse the date into a date object
-			try:
-				postContext['date'] = datetime.datetime.strptime(postContext['date'], '%d-%m-%Y')
-			except Exception, e:
-				logging.warning("Date format not correct for page %s, should be dd-mm-yy\n%s" % (page.path, e))
-				continue
+            postContext = {}
+            postContext['title'] = find('title')
+            postContext['author'] = find('author')
+            postContext['date'] = find('date')
+            postContext['path'] = page.path
+            postContext['tease'] = find('tease')
 
-			POSTS.append(postContext)
+            # Parse the date into a date object
+            try:
+                postContext['date'] = datetime.datetime.strptime(postContext['date'], '%d-%m-%Y')
+            except Exception, e:
+                logging.warning("Date format not correct for page %s, should be dd-mm-yy\n%s" % (page.path, e))
+                continue
 
-	# Sort the posts by date
-	POSTS = sorted(POSTS, key=lambda x: x['date'])
-	POSTS.reverse()
+            POSTS.append(postContext)
 
-	indexes = xrange(0, len(POSTS))
+    # Sort the posts by date
+    POSTS = sorted(POSTS, key=lambda x: x['date'])
+    POSTS.reverse()
 
-	for i in indexes:
-		if i+1 in indexes: POSTS[i]['prevPost'] = POSTS[i+1]
-		if i-1 in indexes: POSTS[i]['nextPost'] = POSTS[i-1]
+    indexes = xrange(0, len(POSTS))
+
+    for i in indexes:
+        if i+1 in indexes: POSTS[i]['prevPost'] = POSTS[i+1]
+        if i-1 in indexes: POSTS[i]['nextPost'] = POSTS[i-1]
 
 
 def preBuildPage(site, page, context, data):
 
-	context['posts'] = POSTS
+    context['posts'] = POSTS
 
-	for post in POSTS:
-		if post['path'] == page.path:
-			context.update(post)
+    for post in POSTS:
+        if post['path'] == page.path:
+            context.update(post)
 
-	return context, data
+    return context, data
